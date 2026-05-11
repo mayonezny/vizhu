1af1893f96a96c1180826f14a792fe64914c86b7
mayonezny <borischubuhchiev@mail.ru>
2026-05-11 14:03:39 +0300

    забыл

--- api/src/modules/ai/ai.module.ts
+++ api/src/modules/ai/ai.module.ts
@@ -3,9 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { AiController } from './ai.controller';
 import { AiService } from './ai.service';
 import { HistoryModule } from '../history/history.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [HttpModule, HistoryModule],
+  imports: [HttpModule, HistoryModule, AuthModule],
   controllers: [AiController],
   providers: [AiService],
   exports: [AiService],
