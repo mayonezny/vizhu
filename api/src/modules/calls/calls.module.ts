ae258c2faddea499ef0b48ec24db7b387501031a
mayonezny <borischubuhchiev@mail.ru>
2026-07-01 12:23:33 +0300

    фиксик

--- api/src/modules/calls/calls.module.ts
+++ api/src/modules/calls/calls.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CallsService } from './calls.service';
 import { CallsController } from './calls.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, AuthModule],
   controllers: [CallsController],
   providers: [CallsService],
   exports: [CallsService], // чтобы модуль матчинга (след. шаг) мог дёргать createToken/ensureRoom
